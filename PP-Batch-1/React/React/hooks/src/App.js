eb412d63fab0d3c803db7fac7a2eafb4230a9eae
mahi-19 <upadhyayvivek.19@gmail.com>
2021-07-02 20:08:40 +0530

    Ue3&Infinte Case Cover

--- PP-Batch-1/React/React/hooks/src/App.js
+++ PP-Batch-1/React/React/hooks/src/App.js
@@ -1,16 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import Us from './UseState/Us';
+import Ue1 from './useEffect/Ue1'; 
 import Ue2 from './useEffect/Ue2';
-// import Ue1 from './useEffect/Ue1';
+import Ue3 from './useEffect/Ue3';
+import Infinite from './useEffect/Infinite';
 function App() {
   return (
 <>
 {/* <h1>Heelo</h1> */}
 {/* <Us/>
  */}
-<Ue2/>
+{/* <Ue2/> */}
 {/* <Ue1/> */}
+
+{/* <Ue3/> */}
+<Infinite />
 </>
   );
 }
